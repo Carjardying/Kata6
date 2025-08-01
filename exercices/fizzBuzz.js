15e4ba3b813e407710cb68a9c4869565617e8a3a
Carjardying <carolinejardy.pro@gmail.com>
2025-08-01 11:20:39 +0200

    final push for fizzbuzz bc i want to do the others and i don't understand the meaning of the 'index starts at 1'

--- exercices/fizzBuzz.js
+++ exercices/fizzBuzz.js
@@ -4,12 +4,12 @@ function fizzbuzz(number) {
   for (let i = 1; i <= number; i++) {
     if (i % 3 == 0 && i % 5 == 0) {   //toujours commecner par celui-ci!!! sinon ça ne focntionne pas
       answer.push("Fizzbuzz");
-    } else if (i % 3 == 0) {
+    } else if (i % 3 == 0) { //on test i et pas number, sinon ça retourne number au lieu de donner les chiffres
       answer.push("Fizz");
     } else if (i % 5 == 0) {
       answer.push("Buzz");
     } else {
-      answer.push(String(i));
+      answer.push(String(i)); 
     }
   }
   return answer;
@@ -18,3 +18,7 @@ console.log(fizzbuzz(3));
 console.log(fizzbuzz(5));
 console.log(fizzbuzz(15));
 
+/* Je ne comprends pas comment mon talbeau est sensné faire
+"La fonction doit retourner un tableau de chaînes de caractères answer (indexé à partir de 1)."
+un tableau commence toujours par l'index 0. 
+Ce sera ma réponse finale, je veux commcner les autres exercices et il est déjà 11h20 */
