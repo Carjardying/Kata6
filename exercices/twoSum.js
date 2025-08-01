d882f03ba501426e7678b42ca2785ac4fa795320
Carjardying <carolinejardy.pro@gmail.com>
2025-08-01 11:42:21 +0200

    should work. i don't undernstand why i don't get the answer but undefined instead

--- exercices/twoSum.js
+++ exercices/twoSum.js
@@ -1,16 +1,15 @@
 let numbers = [2, 7, 11, 15]; // pas const sinon je ne pourrai pas en changer plus tard
 let target = 9;
 
-
-function twoSum(numbers, target){
-    for (let i = 0 && let j = i+1; i < numbers.length && j<numbers.lenght; i++ && j++) {  /*il faut addiditonner deux nomber du tableau pour avoir target donc i et j*/
-    if (numbers[i] + numbers[j] === target) {
-          return [i, j];
+function twoSum(numbers, target) {
+  /*il faut addiditonner deux nomber du tableau pour avoir target donc i et j*/
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.lenght; j++) { /* j = 1+i pour as avoir deux fois le meme nombre */
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
     }
-    }
-
+  }
 }
 
-console.log(twoSum([2, 7, 11, 15], 9));
-
-
+console.log(twoSum(numbers, target));
